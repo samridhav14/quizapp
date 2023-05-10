ba9a1c7776b3b00897207ca3f0ebea90a76c0d41
samridhav14 <100757567+samridhav14@users.noreply.github.com>
2023-05-10 23:44:01 +0530

    hello app commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MyApp());
-}
+// void main(){
+//   runApp(MyApp());
+// }
+void main() => runApp(MyApp()); // alternative way of writnig void main
 class MyApp extends StatelessWidget{
+  @override //its a decorator its there to make code cleaner here over
+  // ride basically means we delibrately over riding build 
   Widget build(BuildContext context){
    return MaterialApp(home: Text(' hello!'),);
   }
