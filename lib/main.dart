67697d99ebcb687cee2dfeede2fbb96b4ddbb19e
samridhav14 <100757567+samridhav14@users.noreply.github.com>
2023-05-11 16:34:06 +0530

    widget tree building scaffold app bar body

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,15 @@ import 'package:flutter/material.dart';
 //   runApp(MyApp());
 // }
 void main() => runApp(MyApp()); // alternative way of writnig void main
-class MyApp extends StatelessWidget{
+
+class MyApp extends StatelessWidget {
   @override //its a decorator its there to make code cleaner here over
-  // ride basically means we delibrately over riding build 
-  Widget build(BuildContext context){
-   return MaterialApp(home: Text(' hello!'),);
+  // ride basically means we delibrately over riding build
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(// scaffold is given by flutter which helps us to include other widgets to get a basic structure
+          appBar: AppBar(title: Text('My First App')),
+          body: Text('this is my default setup')),
+    );
   }
 }
