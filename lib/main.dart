6977d4b76a0617f951421cb78a6dd041b892c599
samridhav14 <100757567+samridhav14@users.noreply.github.com>
2023-05-11 17:26:16 +0530

    column widget and buttons added for quiz app

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,29 @@ class MyApp extends StatelessWidget {
   // ride basically means we delibrately over riding build
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(// scaffold is given by flutter which helps us to include other widgets to get a basic structure
-          appBar: AppBar(title: Text('My First App')),
-          body: Text('this is my default setup')),
+      home: Scaffold(
+        // scaffold is given by flutter which helps us to include other widgets to get a basic structure
+        appBar: AppBar(title: Text('My First App')),
+        /* the problem with body is it cant have multiple widgets in it there for instead of multiple text we will use a column widget which will contain a list of widgets*/
+        // body: Text('this is my default setup')),
+        body: Column(
+          children: [
+            Text('    the question?   '),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Option 1'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Option 2'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Option 3'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
