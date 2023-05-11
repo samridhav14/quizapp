d43771ba57fc610751cceffcdc22d92cbaec28f0
samridhav14 <100757567+samridhav14@users.noreply.github.com>
2023-05-11 18:17:13 +0530

    funtion by refrence and const keyword added

--- lib/main.dart
+++ lib/main.dart
@@ -9,26 +9,29 @@ class MyApp extends StatelessWidget {
   @override //its a decorator its there to make code cleaner here over
   // ride basically means we delibrately over riding build
   Widget build(BuildContext context) {
+    void ansquestion(){
+       print('question answered');
+    }
     return MaterialApp(
       home: Scaffold(
         // scaffold is given by flutter which helps us to include other widgets to get a basic structure
-        appBar: AppBar(title: Text('My First App')),
+        appBar: AppBar(title: const Text('My First App')),
         /* the problem with body is it cant have multiple widgets in it there for instead of multiple text we will use a column widget which will contain a list of widgets*/
         // body: Text('this is my default setup')),
         body: Column(
           children: [
-            Text('    the question?   '),
+            const Text('    the question?   '),
             ElevatedButton(
-              onPressed: null,
-              child: Text('Option 1'),
+              onPressed: ansquestion, // here we are passing refrence istead of function so that it get executed when button is pressed
+              child: const Text('Option 1'),
             ),
             ElevatedButton(
-              onPressed: null,
-              child: Text('Option 2'),
+              onPressed: ansquestion,
+              child:const Text('Option 2'),
             ),
             ElevatedButton(
-              onPressed: null,
-              child: Text('Option 3'),
+              onPressed: ansquestion,
+              child:const Text('Option 3'),
             ),
           ],
         ),
