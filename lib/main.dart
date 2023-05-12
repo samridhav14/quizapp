9268c9a5e5b6680c04068d5852ea62305e10cb9a
samridhav14 <100757567+samridhav14@users.noreply.github.com>
2023-05-12 11:00:08 +0530

    statefull widget is created

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,34 @@ import 'package:flutter/material.dart';
 // }
 void main() => runApp(MyApp()); // alternative way of writnig void main
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  // const MyApp({super.key});
+  @override
+  State<MyApp> createState() {
+    return MyAppState();
+  }
+  // State<MyApp> createState() => _MyAppState();
+}
+
+// we have two class because when the external data changes Myapp class can be recreated how ever state is persistent it is connected to myapp class but technically it is not recreated just the widget tree is rebuilt
+class MyAppState extends State<MyApp> {
+  //widgetname+state
+  var questionIndex = 0;
   @override //its a decorator its there to make code cleaner here over
   // ride basically means we delibrately over riding build
   Widget build(BuildContext context) {
-    void ansquestion(){
-       print('question answered');
+    void ansquestion() {
+      print(questionIndex);
+      setState(() {
+        questionIndex = questionIndex + 1;
+        print(questionIndex);
+      });
     }
+
+    var questions = [
+      'Who is better cheems or dogesh',
+      'Which company\'s shoes vimdhayak ji have?'
+    ];
     return MaterialApp(
       home: Scaffold(
         // scaffold is given by flutter which helps us to include other widgets to get a basic structure
@@ -20,24 +41,28 @@ class MyApp extends StatelessWidget {
         // body: Text('this is my default setup')),
         body: Column(
           children: [
-            const Text('    the question?   '),
+            Text(questions[questionIndex]),
             ElevatedButton(
-              onPressed: ansquestion, // here we are passing refrence istead of function so that it get executed when button is pressed
+              onPressed:
+                  ansquestion, // here we are passing refrence istead of function so that it get executed when button is pressed
               child: const Text('Option 1'),
             ),
             ElevatedButton(
-              onPressed: (){
-                // here we have used annonymous function because this function will not be called from anywhere else
-                print('ans 2 choosen');
-              },
-              child:const Text('Option 2'),
+              onPressed: ansquestion //() {
+              //   // here we have used annonymous function because this function will not be called from anywhere else
+              //   ansquestion;
+              // }
+              ,
+              child: const Text('Option 2'),
             ),
             ElevatedButton(
-              onPressed: (){
-                // it is another way of rpresenting the refrence of function thing both will gave simmilar output
-                print('ans 3 choosen');
-              },
-              child:const Text('Option 3'),
+              onPressed: ansquestion
+              //() {
+              // it is another way of rpresenting the refrence of function thing both will gave simmilar output
+              // ansquestion;
+              //}
+              ,
+              child: const Text('Option 3'),
             ),
           ],
         ),
