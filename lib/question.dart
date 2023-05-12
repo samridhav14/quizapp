f4b1838466e5a01a14410a9c0640f41653be5965
samridhav14 <100757567+samridhav14@users.noreply.github.com>
2023-05-12 16:37:57 +0530

    styling question widget and using container widget

--- lib/question.dart
+++ lib/question.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
-
 class Question extends StatelessWidget {
-  final String questionText; // it is to ensure that once a string is initialized for a object we cant change it 
-   Question(this.questionText);
+  final String
+      questionText; // it is to ensure that once a string is initialized for a object we cant change it
+  Question(this.questionText);
   @override
   Widget build(BuildContext context) {
     // here this get rebuild everythime the data that is coming from main.dart changes hence statelesswidgets get rebuilt on input change thing is true
-    return Text(questionText);
+    return Container(
+      width: double.infinity,
+     margin:EdgeInsets.all(10),
+    child:Text(
+      questionText,
+      style: TextStyle(fontSize: 24),
+     textAlign: TextAlign.center ,
+    ),);
   }
 }
+
