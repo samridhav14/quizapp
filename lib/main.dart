e20e2689f0555d26e023e6e467893c0f81c69e3b
samridhav14 <100757567+samridhav14@users.noreply.github.com>
2023-05-13 20:48:01 +0530

    setting up for if else

--- lib/main.dart
+++ lib/main.dart
@@ -19,19 +19,8 @@ class MyApp extends StatefulWidget {
 // we have two class because when the external data changes Myapp class can be recreated how ever state is persistent it is connected to myapp class but technically it is not recreated just the widget tree is rebuilt
 class _MyAppState extends State<MyApp> {
   //widgetname+state
-  var _questionIndex = 0;
-  void _ansquestion() {
-    setState(() {
-      _questionIndex = _questionIndex + 1;
-    });
-  }
-
-  @override //its a decorator its there to make code cleaner here over
-  // ride basically means we delibrately over riding build
-  Widget build(BuildContext context) {
-    // here it is compile time constant a thing which is compile time contant is also run time constant
- // we can add const before a value also but both of them are different if we riassign the variable it will work differently
- const  List<Map<String, dynamic>> questions = [
+  // now we cant use const directly we need to set it as final
+  final  List<Map<String, dynamic>> questions = const[
       // right now only question are there now use maps which is a key value data structure
       {
         'question': 'Who is better cheems or dogesh',
@@ -46,14 +35,49 @@ class _MyAppState extends State<MyApp> {
         'answer': ['chimki', 'dogelina', 'vimdhayak\'s wife']
       }
     ];
-    // question =[]; this does not work
+  
+  var _questionIndex = 0;
+  void _ansquestion() {
+    // questions is unidentified we need to take it out of built class
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+  }
+
+  @override //its a decorator its there to make code cleaner here over
+  // ride basically means we delibrately over riding build
+  Widget build(BuildContext context) {
+    // here it is compile time constant a thing which is compile time contant is also run time constant
+ // we can add const before a value also but both of them are different if we riassign the variable it will work differently
+//  const  List<Map<String, dynamic>> questions = [
+//       // right now only question are there now use maps which is a key value data structure
+//       {
+//         'question': 'Who is better cheems or dogesh',
+//         'answer': ['I love cheems', 'I love dogesh', 'Both are equal'],
+//       },
+//       {
+//         'question': 'Which company\'s shoes vimdhayak ji have?',
+//         'answer': ['nmike', 'poma', 'amdibas']
+//       },
+//       {
+//         'question': 'who is more beautiful',
+//         'answer': ['chimki', 'dogelina', 'vimdhayak\'s wife']
+//       }
+//     ];
+     // question =[]; this does not work
     return MaterialApp(
       home: Scaffold(
         // scaffold is given by flutter which helps us to include other widgets to get a basic structure
         appBar: AppBar(title: const Text('Cheems Quiz')),
         /* the problem with body is it cant have multiple widgets in it there for instead of multiple text we will use a column widget which will contain a list of widgets*/
         // body: Text('this is my default setup')),
-        body: Column(
+        body: 
+        
+        
+        
+        
+        
+        Column(
           children: [
             Question(
               questions[_questionIndex]['question'],
